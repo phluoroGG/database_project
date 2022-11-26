c823cf0347dac0215cf1a3010d0d7b618225664b
phluoroGG <phluoro@gmail.com>
2022-11-26 03:00:53 +0300

    Python Flask SQLAlchemy project

---
+++ app_config.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:root@localhost/vetrov_k_a?auth_plugin=mysql_native_password"
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = False # Autocommit
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # ??? Если не прописать, то будет Warning
+    SECRET_KEY = 'we4fh%gC_za:*8G5v=fbv'
+
+
+
+
+app = Flask(__name__)
+app.config.from_object(Config())
+db = SQLAlchemy(app=app, session_options={'autoflush': False})
